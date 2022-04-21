775724e71783d1742b87aa2651da8f6685cdbfac
sergio costa <sergio.costa@ufma.br>
2022-04-21 14:43:03 -0300

    iniciando ...

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -1,13 +1,28 @@
 package br.ufma.ecp;
 
-/**
- * Hello world!
- *
- */
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class App 
 {
+
+    private static String fromFile() {
+        File file = new File("Main.jack");
+
+        byte[] bytes;
+        try {
+            bytes = Files.readAllBytes(file.toPath());
+            String textoDoArquivo = new String(bytes, "UTF-8");
+            return textoDoArquivo;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    } 
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( fromFile() );
     }
 }
