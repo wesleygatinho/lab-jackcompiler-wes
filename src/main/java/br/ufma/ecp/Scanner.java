645b8b87a8382411e4719efdbd23ee3840c6cb1e
wesleygatinho <wesley.gatinho@discente.ufma.br>
2024-02-07 16:52:16 -0300

    Incluindo SkipLine, SkipBlock, peelNext e alterando comando "/"

--- src/main/java/br/ufma/ecp/Scanner.java
+++ src/main/java/br/ufma/ecp/Scanner.java
@@ -77,8 +77,17 @@ public class Scanner {
 
         switch (ch) {
             case '/':
+            if (peekNext() == '/') {
+                skipLineComments();
+                return nextToken();
+            } else if (peekNext() == '*') {
+                skipBlockComments();
+                return nextToken();
+            }
+            else {
                 advance();
                 return new Token (TokenType.SLASH,"/");
+            }
 
             case '+':
                 advance();
@@ -199,6 +208,42 @@ public class Scanner {
            return (char)input[current];
        return 0;
     }
+
+    private void skipLineComments() {
+        for (char ch = peek(); ch != '\n' && ch != 0;  advance(), ch = peek()) ;
+    }
+
+    private void skipBlockComments() {
+        boolean endComment = false;
+        advance();
+        while (!endComment) {
+            advance();
+            char ch = peek();
+            if ( ch == 0) { // eof, lexical error
+                System.exit(1);
+            }
+         
+            if (ch == '*') {
+               for (ch = peek(); ch == '*';  advance(), ch = peek()) ;
+                if (ch == '/') {
+                    endComment = true;
+                    advance();
+                }
+            }
+
+        }
+    }
+
+    private char peekNext () {
+        int next = current + 1;
+        if ( next  < input.length) {
+            return (char)input[next];
+        } else {
+            return 0;
+        }
+   }
+
+ 
 
 
     
