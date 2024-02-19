c1f4fc869128cf4328ddd50d893c07e286745351
wesleygatinho <wesley.gatinho@discente.ufma.br>
2024-02-19 19:05:49 -0300

    Literais keyword: com testes em false, null, true e this

--- src/main/java/br/ufma/ecp/Parser.java
+++ src/main/java/br/ufma/ecp/Parser.java
@@ -343,9 +343,13 @@ public class Parser {
             case NULL:
             case TRUE:
                 expectPeek(FALSE, NULL, TRUE);
+                vmWriter.writePush(Segment.CONST, 0);
+                if (currentToken.type == TRUE)
+                    vmWriter.writeArithmetic(Command.NOT);
                 break;
             case THIS:
                 expectPeek(THIS);
+                vmWriter.writePush(Segment.POINTER, 0);
                 break;
             case IDENT:
                 expectPeek(IDENT);
